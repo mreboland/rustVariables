4d030febcf3c0d59349c57f325e3088c80506fd1
Edward Boland <mr.eboland@gmail.com>
2021-05-03 17:53:53 -0400

    using variables and println

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let a_number = 10;
+    let a_boolean = true;
+
+    // println! macro takes a string as a first argument and one extra argument for each curly bracket pair inside the string and replaces them for the argument's display value.
+    println!("The number is {}.", a_number);
+    println!("The boolean is {}.", a_boolean);
 }
